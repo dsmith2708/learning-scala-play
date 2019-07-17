fc829499b3cd845b54837f2cff3a7ea8ee4be6b8
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-17 15:22:40 +0100

    implemented error handler

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,10 @@
 package controllers
 
+import models.People
 import play.api._
 import play.api.mvc._
+
+import scala.collection.mutable.ListBuffer
 
 class Application extends Controller {
 
@@ -11,6 +14,17 @@ class Application extends Controller {
 
   def form = Action {
     Ok(views.html.form())
+  }
+
+  def list = Action {
+    val listItems: ListBuffer[People] = ListBuffer(
+      new People("John Waters", 15),
+      new People("James Wick", 25),
+      new People("Jimmy West", 35),
+      new People("John Wick", 45)
+    )
+
+    Ok(views.html.listDisplay(listItems))
   }
 
 }
---
+++ app/models/People.scala
@@ -0,0 +1,5 @@
+package models
+
+import scala.collection.mutable.ListBuffer
+
+class People(val name: String, val age: Int)
