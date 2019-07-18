4a569280e27fad1ebca086aafa44d3d4f09a193e
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-18 09:41:53 +0100

    added example of reading from session in controller, prints email found

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,30 +1,50 @@
 package controllers
 
-import models.People
-import play.api._
+import javax.inject.Inject
+import models.{CD, People}
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import models.CD._
 
-import scala.collection.mutable.ListBuffer
-
-class Application extends Controller {
+class Application @Inject()(val messagesApi: MessagesApi, environment: play.api.Environment) extends Controller with I18nSupport {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your new application is ready.")).withSession(("user", "user@gmail.com"))
   }
-
-  def form = Action {
-    Ok(views.html.form())
-  }
-
+//
+//  def formFunction = Action {
+//    Ok(views.html.form(People.peopleFormConstraints))
+//  }
+//
   def list = Action {
-    val listItems: ListBuffer[People] = ListBuffer(
-      new People("John Waters", 15),
-      new People("James Wick", 25),
-      new People("Jimmy West", 35),
-      new People("John Wick", 45)
-    )
-
-    Ok(views.html.listDisplay(listItems))
+    Ok(views.html.listDisplay(CD.createCDForm, CD.cds, "List view page"))
   }
+
+
+  def createCD() = Action { implicit request =>
+    val formValidationResult = CD.createCDForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      println(request.session.get("user").getOrElse("TitleNotFound"))
+      BadRequest(views.html.listDisplay(CD.createCDForm, CD.cds, request.session.get("user").getOrElse("TitleNotFound")))
+    }, { cd =>
+      println(request.session.get("user").getOrElse("TitleNotFound"))
+      CD.cds.append(cd)
+      Redirect(routes.Application.list())
+    })
+  }
+
+
+//  def sayHello() = Action { implicit request =>
+//    val formData = People.peopleFormConstraints.bindFromRequest
+//    formData.fold(
+//      {
+//        formWithErrors => BadRequest(views.html.listDisplay())
+//      },
+//      {
+//        formData =>
+//        Ok(views.html.main(s"posted with x.name: ${formData.name}, x.age: ${formData.age}"))
+//      }
+//    )
+//   }
 
 }
--- app/models/People.scala
+++ app/models/People.scala
@@ -1,5 +1,25 @@
 package models
 
+import play.api.data._
+import play.api.data.Forms._
 import scala.collection.mutable.ListBuffer
 
-class People(val name: String, val age: Int)
+case class People(name: String, age: Int)
+
+object People{
+
+  val peopleFormConstraints = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "age" -> number(min = 0, max = 100)
+    )(People.apply)(People.unapply)
+  )
+
+  val listItems: ListBuffer[People] = ListBuffer(
+    new People("John Waters", 15),
+    new People("James Wick", 25),
+    new People("Jimmy West", 35),
+    new People("John Wick", 45)
+  )
+
+}
